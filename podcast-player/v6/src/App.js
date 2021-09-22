77ae7ffd21bfe59e3582c184e9355c6469982434
Alan Richardson <alan.richardson@ag-grid.com>
2021-09-22 14:09:17 +0100

    v6 pagination

---
+++ podcast-player/v6/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, {useState} from 'react';
+import {PodcastGrid} from './PodcastGrid';
+
+function App() {
+
+  const [rssFeed, setRssFeed] = useState("https://feeds.simplecast.com/tOjNXec5");
+  const [quickFilter, setQuickFilter] = useState("");
+
+  const handleLoadFeedClick = ()=>{
+    const inputRssFeed = document.getElementById("rssFeedUrl").value;
+    setRssFeed(inputRssFeed);
+  }
+
+  const handleFilterChange = (event)=>{
+    setQuickFilter(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <h1>Podcast Player</h1>
+      <div>
+        <label htmlFor="rssFeedUrl">RSS Feed URL:</label>
+        <input type="text" id="rssFeedUrl" name="rssFeedUrl"  style={{width:"50%"}} defaultValue={rssFeed}/>
+        <button onClick={handleLoadFeedClick}>Load Feed</button>
+      </div>
+      <div>
+      <label htmlFor="quickfilter">Quick Filter:</label>
+        <input type="text" id="quickfilter" name="quickfilter" style={{width:"30%"}} value={quickFilter}
+              onChange={handleFilterChange}/>        
+      </div>
+      <div>
+        <PodcastGrid
+        rssfeed = {rssFeed}
+        height= "800px"
+        width="100%"     
+        quickFilter = {quickFilter}   
+      ></PodcastGrid>
+      </div>
+    </div>
+  );
+}
+
+export default App;
