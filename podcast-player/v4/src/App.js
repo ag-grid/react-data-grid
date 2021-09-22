73b01b3326824ee248ecc6e1219ea554df570e98
Alan Richardson <alan.richardson@ag-grid.com>
2021-09-22 09:27:55 +0100

    added v4 code

---
+++ podcast-player/v4/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, {useState} from 'react';
+import {PodcastGrid} from './PodcastGrid';
+
+function App() {
+
+  const [rssFeed, setRssFeed] = useState("https://feeds.simplecast.com/tOjNXec5");
+
+  const handleLoadFeedClick = ()=>{
+    const inputRssFeed = document.getElementById("rssFeedUrl").value;
+    setRssFeed(inputRssFeed);
+  }
+
+  return (
+    <div className="App">
+      <h1>Podcast Player</h1>
+      <div>
+        <label htmlFor="rssFeedUrl">RSS Feed URL:</label>
+        <input type="text" id="rssFeedUrl" name="rssFeedUrl"  width="80%" defaultValue={rssFeed}/>
+        <button onClick={handleLoadFeedClick}>Load Feed</button>
+      </div>
+      <PodcastGrid
+        rssfeed = {rssFeed}
+        height= "400px"
+        width="100%"
+      ></PodcastGrid>
+    </div>
+  );
+}
+
+export default App;
