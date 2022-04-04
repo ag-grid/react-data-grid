d35fca9693f2ff7a1ebe27eaff58796f9f47b754
Alan Richardson <alan.richardson@ag-grid.com>
2022-04-04 15:48:36 +0100

    Update builddocs.sh

--- getting-started-video-tutorial/docs/builddocs.sh
+++ getting-started-video-tutorial/docs/builddocs.sh
@@ -16,6 +16,6 @@ cat 007-react-custom-floating-filters.md >> react-tutorial.md
 cat newpage.md >> react-tutorial.md
 cat 008-react-components.md >> react-tutorial.md
 cat newpage.md >> react-tutorial.md
-cat 009-react-components.md >> react-tutorial.md
+cat 009-updating-row-data.md >> react-tutorial.md
 pandoc react-tutorial.md -o react-tutorial-workbook.pdf --from markdown --template eisvogel --listings --toc
 open react-tutorial-workbook.pdf
