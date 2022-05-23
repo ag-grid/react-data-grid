2cb33f6a23ca2fbd08b0ae7f747011c221418b1d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-23 18:22:04 +0800

    Delete podcast-player directory

--- podcast-player/v2/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the app', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Podcast Player/i);
-  expect(headerElement).toBeInTheDocument();
-});
